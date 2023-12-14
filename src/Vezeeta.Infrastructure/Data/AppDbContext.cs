468e3209c10a933e2ec44ad27bc27a75424be4e9
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-12-14 21:11:13 +0200

    Added Basic EF Core Config

--- src/Vezeeta.Infrastructure/Data/AppDbContext.cs
+++ src/Vezeeta.Infrastructure/Data/AppDbContext.cs
@@ -1,8 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using Vezeeta.Domain.Entities;
 
 namespace Vezeeta.Infrastructure.Data;
 
 public sealed class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
 
+    public DbSet<Doctor> Doctors { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 }
--- src/Vezeeta.Infrastructure/InfrastructureExtension.cs
+++ src/Vezeeta.Infrastructure/InfrastructureExtension.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Vezeeta.Infrastructure.Data;
 
 namespace Vezeeta.Infrastructure;
 
@@ -7,6 +9,10 @@ public static class InfrastructureExtension
 {
     public static IServiceCollection RegisterInfrastructureServices(this IServiceCollection services, IConfiguration config)
     {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+        });
 
         return services;
     }
