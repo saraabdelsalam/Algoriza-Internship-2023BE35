086447fd85da21b871c83fafd70a1e29c9da7df5
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-12-17 00:18:28 +0200

    Fix endpoints not shown on Swagger UI

--- src/Vezeeta.API/Endpoints/DashboardEndpoints.cs
+++ src/Vezeeta.API/Endpoints/DashboardEndpoints.cs
@@ -10,23 +10,28 @@ public static class DashboardEndpoints
         adminDashboardGroup.MapGet("/Top10Doctors", async (HttpContext context) =>
         {
             await context.Response.WriteAsync("Test");
+            return Results.Ok();
         });
 
         adminDashboardGroup.MapGet("/Top5Specializations", async (HttpContext context) =>
         {
             await context.Response.WriteAsync("");
+            return Results.Ok();
         });
         adminDashboardGroup.MapGet("/TotalNumberOfDoctors", async (HttpContext context) =>
         {
             await context.Response.WriteAsync("");
+            return Results.Ok();
         });
         adminDashboardGroup.MapGet("/TotalNumberOfPatients", async (HttpContext context) =>
         {
             await context.Response.WriteAsync("");
+            return Results.Ok();
         });
         adminDashboardGroup.MapGet("/TotalNumberOfRequests", async (HttpContext context) =>
         {
             await context.Response.WriteAsync("");
+            return Results.Ok();
         });
 
         return app;
